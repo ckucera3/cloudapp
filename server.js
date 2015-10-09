be1d9b43ae6ef00693d29668512e7c050156c5b2
Clayton <claytonkucera@gmail.com>
2015-10-08 20:38:55 -0400

    initial node and express structure, plus simple routing

---
+++ server.js
@@ -0,0 +1,25 @@
+/*jslint node: true */
+'use strict';
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+//configure express
+app.set('view engine', 'ejs');
+
+//set up initial routing
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/app/index.html'));
+});
+
+//start the server
+var server = app.listen(port, function () {
+    console.log("listening on port " + port);
+});
+
+
+
+module.exports = app;
