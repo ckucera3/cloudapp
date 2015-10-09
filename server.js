f717560e7dfc661b40e8bbc582ca97bd86d88d35
clayton <claytonkucera@gmail.com>
2015-10-09 07:42:31 -0400

    changed port number for openshift config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 //configure express
 app.set('view engine', 'ejs');
