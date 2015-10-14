dc56601a043bfb749166a9425db1a620befee630
Clayton <claytonkucera@gmail.com>
2015-10-14 00:52:08 -0400

    added styling; adding views using handlebars; added simple json data for views to use

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 var express = require("express");
 var path = require("path");
+var hbs = require("hbs");
+
 
 var app = express();
 
@@ -12,11 +14,23 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
 //configure express
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', __dirname + '/views');
+app.use(express.static('./public'));
+
+
 
 //set up initial routing
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/app/index.html'));
+    res.render('index');
+});
+
+//get cloud data
+var clouds = require('./public/javascripts/clouds.json');
+
+app.get('/clouds', function (req, res) {
+   res.render('clouds', clouds);
 });
 
 //start the server
