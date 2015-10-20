b395bce688451245ce51ab8a9841a9648dfd2f16
clayton <claytonkucera@gmail.com>
2015-10-20 11:18:00 -0400

    added form handling and cloud styles.

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-/*jslint node: true */
 'use strict';
 
 var express = require("express");
 var path = require("path");
 var hbs = require("hbs");
-
-
+var bodyParser = require("body-parser");
 var app = express();
 
 //configure env variables
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-
 //configure express
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', __dirname + '/views');
 app.use(express.static('./public'));
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //set up initial routing
 app.get('/', function (req, res) {
@@ -31,6 +28,20 @@ var clouds = require('./public/javascripts/clouds.json');
 
 app.get('/clouds', function (req, res) {
    res.render('clouds', clouds);
+});
+
+app.post('/clouds', function(req, res) {
+	console.log(req.body);
+	if (req.body.cloudName != undefined && req.body.cloudDescription != undefined) {
+		var newCloud = {
+			name: req.body.cloudName,
+			description: req.body.cloudDescription
+		};
+		console.log(newCloud);
+		clouds.clouds.push(newCloud);
+		    res.render('clouds', clouds);
+
+	}
 });
 
 //start the server
